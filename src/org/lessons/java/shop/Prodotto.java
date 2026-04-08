91b4361c0e04a9c2846c287ec764fc926413b5e3
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-04-08 18:26:38 +0200

    creato metodo costruttore della classe Prodotto

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,5 +1,17 @@
 package org.lessons.java.shop;
 
 public class Prodotto {
-    
+    public int codice;
+    public String nome;
+    public String descrizione;
+    public float prezzo;
+    public float iva;
+
+    public Prodotto(int codice, String nome, String descrizione, float prezzo, float iva) {
+        this.codice = codice;
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.prezzo = prezzo;
+        this.iva = iva;
+    }
 }
