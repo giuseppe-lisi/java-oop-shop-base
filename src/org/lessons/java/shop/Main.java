234603754cfdf5deb789ac098bf3fac65fcbdc92
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-04-08 19:00:02 +0200

    min

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -3,10 +3,9 @@ package org.lessons.java.shop;
 public class Main {
     public static void main(String[] args) {
         Prodotto banana = new Prodotto("banana", "è un frutto giallo simile ad una zucchina", 20f, 10);
-        
 
         System.out.println("Costo base: " + banana.getBasePrice());
         System.out.println("Costo comprensivo di iva: " + banana.getTaxedPrice());
-        System.out.println("Codice e nome prodotto" + banana.getProductName());
+        System.out.println("Codice e nome prodotto: " + banana.getProductName());
     }
 }
