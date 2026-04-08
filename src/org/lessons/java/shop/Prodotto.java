c0c20b4c4e00bccdca0b1e20e8c586c385d42369
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-04-08 18:59:44 +0200

    gli attributi della classe Prodotto non sono più esposti e possono essere settati solo dal metodo costruttore

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -3,11 +3,11 @@ package org.lessons.java.shop;
 import java.util.Random;
 
 public class Prodotto {
-    public int codice;
-    public String nome;
-    public String descrizione;
-    public float prezzo;
-    public float iva;
+    private int codice;
+    private String nome;
+    private String descrizione;
+    private float prezzo;
+    private float iva;
 
     public Prodotto(String nome, String descrizione, float prezzo, float iva) {
         // genera un codice random compreso tra 10 e 50 inclusive
@@ -20,17 +20,17 @@ public class Prodotto {
         this.iva = iva;
     }
 
-    float getBasePrice() {
+    public float getBasePrice() {
         return this.prezzo;
     }
 
-    float getTaxedPrice() {
+    public float getTaxedPrice() {
         float productIva = this.prezzo * (this.iva / 100);
         float taxedPrice = this.prezzo + productIva;
         return taxedPrice;
     }
 
-    String getProductName() {
+    public String getProductName() {
         return this.codice + "-" + this.nome;
     }
 }
