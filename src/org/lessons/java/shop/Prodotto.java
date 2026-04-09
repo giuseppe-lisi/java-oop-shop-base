56fd6d8c0af066ca5c9e870b09f158bf0393fbe7
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-04-09 09:47:41 +0200

    min

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -12,8 +12,7 @@ public class Prodotto {
     public Prodotto(String nome, String descrizione, float prezzo, float iva) {
         // genera un codice random compreso tra 10 e 50 inclusive
         Random rand = new Random();
-        int codice = rand.nextInt(10,51);
-        this.codice = codice;
+        this.codice = rand.nextInt(1001);;
         this.nome = nome;
         this.descrizione = descrizione;
         this.prezzo = prezzo;
